f094586118f1b37e1d0f40110dec9a75181a3a64
lexxsm <129339424+lexxsm@users.noreply.github.com>
2023-04-08 00:52:57 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+n = int(input())
+res = 0
+i = 0
+v = 0
+w = 0
+nok = []
+while i in range(n):
+    nok.append(input())
+for i in range(n):
+    v = v + 1
+    w = w +1
+  res =  def gcd(nok[w], nok[v])     
+print(res)
